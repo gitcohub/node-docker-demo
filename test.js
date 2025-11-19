dc71caa2190bdd9371dc8772d78acbd16a457b12
Rajdeep Dey <146769108+gitcohub@users.noreply.github.com>
2025-11-20 04:40:13 +0530

    Initial commit - Node Docker demo

---
+++ test.js
@@ -0,0 +1,11 @@
+﻿const request = require('supertest');
+const app = require('./app');
+
+describe('GET /', function() {
+  it('responds with Hello From DSCE', function(done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect(/Hello From DSCE/, done);
+  });
+});
