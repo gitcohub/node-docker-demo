2ae61deb2627176830e75e1dd5a56450cd980f44
Rajdeep Dey <146769108+gitcohub@users.noreply.github.com>
2025-11-20 06:24:06 +0530

    Add /test endpoint to verify CI/CD

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const app = express();
 app.get('/', (req, res) => res.send('{ "response": "Hello From DSCE" }'));
 app.get('/will', (req, res) => res.send('{ "response": "Hello World" }'));
 app.get('/ready', (req, res) => res.send('{ "response": "Great!, It works!" }'));
+app.get('/test', (req, res) => res.send('{ "response": "CI/CD is working!" }'));
 module.exports = app;
